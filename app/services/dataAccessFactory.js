21a333a7c2d1b96b4bda757554eb7e553e382787
Deryx <deryxescott@yahoo.com>
2015-01-06 14:07:29 -0500

    Update dataAccessFactory.js

--- app/services/dataAccessFactory.js
+++ app/services/dataAccessFactory.js
@@ -3,7 +3,7 @@
  */
 (function() {
     var DataAccessFactory = function ($http) {
-        var urlBase = 'app/data';
+        var urlBase = 'https://github/Deryx/npc/app/data';
         var factory = {};
 
         factory.getJSONData = function(tableFile) {
